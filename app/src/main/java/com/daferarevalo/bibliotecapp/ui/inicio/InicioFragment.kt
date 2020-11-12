f6f16ab1aaad9e348a5c617a7563a251f8940277
Dario Arevalo <daferd19@gmail.com>
2020-11-12 12:13:39 -0500

    Initial commit

---
+++ app/src/main/java/com/daferarevalo/bibliotecapp/ui/inicio/InicioFragment.kt
@@ -0,0 +1,34 @@
+package com.daferarevalo.bibliotecapp.ui.inicio
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.daferarevalo.bibliotecapp.R
+
+
+class InicioFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_inicio, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    companion object {
+
+    }
+}
---
+++ app/src/main/java/com/daferarevalo/bibliotecapp/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.daferarevalo.bibliotecapp.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.daferarevalo.bibliotecapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
