c23fe266b1987ac828139f4417b4aa5c30334603
Dario Arevalo <daferd19@gmail.com>
2020-11-12 16:02:39 -0500

    Implementacion ViewBinding

--- app/src/main/java/com/daferarevalo/bibliotecapp/ui/inicio/InicioFragment.kt
+++ app/src/main/java/com/daferarevalo/bibliotecapp/ui/inicio/InicioFragment.kt
@@ -1,20 +1,16 @@
 package com.daferarevalo.bibliotecapp.ui.inicio
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.daferarevalo.bibliotecapp.R
+import com.daferarevalo.bibliotecapp.databinding.FragmentInicioBinding
 
 
 class InicioFragment : Fragment() {
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private lateinit var binding: FragmentInicioBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,9 +22,9 @@ class InicioFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-    }
-
-    companion object {
+        binding = FragmentInicioBinding.bind(view)
 
     }
+
+    companion object
 }
--- app/src/main/java/com/daferarevalo/bibliotecapp/ui/login/LoginActivity.kt
+++ app/src/main/java/com/daferarevalo/bibliotecapp/ui/login/LoginActivity.kt
@@ -1,12 +1,18 @@
 package com.daferarevalo.bibliotecapp.ui.login
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.daferarevalo.bibliotecapp.R
+import androidx.appcompat.app.AppCompatActivity
+import com.daferarevalo.bibliotecapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
     }
 }
