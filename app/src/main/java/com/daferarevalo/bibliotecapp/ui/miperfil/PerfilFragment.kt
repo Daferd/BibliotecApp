c2b035545959d2b9a5cf526ca8bedb16487ec0e6
Dario Arevalo <daferd19@gmail.com>
2020-11-16 23:58:28 -0500

    Implementacion de actualizacion de correo en Firebase

--- app/src/main/java/com/daferarevalo/bibliotecapp/ui/miperfil/PerfilFragment.kt
+++ app/src/main/java/com/daferarevalo/bibliotecapp/ui/miperfil/PerfilFragment.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.daferarevalo.bibliotecapp.R
 import com.daferarevalo.bibliotecapp.databinding.FragmentPerfilBinding
 import com.daferarevalo.bibliotecapp.server.Usuario
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
@@ -18,6 +20,11 @@ import com.google.firebase.database.ValueEventListener
 class PerfilFragment : Fragment() {
 
     private lateinit var binding: FragmentPerfilBinding
+
+    //private val TAG = PerfilFragment::class.java.simpleName
+
+    //RegistroActivity::class.simpleName
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,7 +44,40 @@ class PerfilFragment : Fragment() {
             buscarEnFirebase(uidUsuario)
         }
 
+        binding.actualizarPerfilButton.setOnClickListener {
+
+            val nuevoNombre = binding.nombrePerfilEditText.text.toString()
+            val nuevoCorreo = binding.correoPerfilEditText.text.toString()
+            val nuevaContrasena = binding.contrasenaPerfilEditText.toString()
+
+            actualizarCorreoFirebase(user, nuevoCorreo)
+            //actualizarDatabaseFirebase(nuevoNombre,nuevoCorreo)
+        }
+
     }
+
+    /*  private fun actualizarDatabaseFirebase(nuevoNombre: String, nuevoCorreo: String) {
+          val database = FirebaseDatabase.getInstance()
+          val myUsuarioRef = database.getReference("usuarios")
+          val childUpdates = HashMap<String, Any>()
+          childUpdates["nombre"] = nuevoNombre
+          childUpdates["correo"] = nuevoCorreo
+          idDeudor?.let { myDeudorRef.child(it).updateChildren(childUpdates) }
+      }*/
+
+    private fun actualizarCorreoFirebase(
+        user: FirebaseUser?,
+        nuevoCorreo: String
+    ) {
+        user!!.updateEmail(nuevoCorreo)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(context, "Actualizado", Toast.LENGTH_SHORT).show()
+                    //Log.d(TAG, "User email address updated.")
+                }
+            }
+    }
+
 
     private fun buscarEnFirebase(uidUsuario: String) {
         val database = FirebaseDatabase.getInstance()
