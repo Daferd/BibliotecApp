e78512c6cf6f19ab169b2e558812603fae0f6506
Dario Arevalo <daferd19@gmail.com>
2020-12-03 16:12:10 -0500

    Implementacion DetalleDialogFragment.kt y vista de Reseña actualizada firebase

--- app/src/main/java/com/daferarevalo/bibliotecapp/ui/detalleDialog/ResenaFragment.kt
+++ app/src/main/java/com/daferarevalo/bibliotecapp/ui/detalleDialog/ResenaFragment.kt
@@ -4,16 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.daferarevalo.bibliotecapp.R
 import com.daferarevalo.bibliotecapp.databinding.FragmentResenaBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 
 class ResenaFragment : Fragment() {
 
     private lateinit var binding: FragmentResenaBinding
+
+    //private var puntuacionlibro : Float = 0.0F
+    //private var cantidadDePuntuaciones : Int = 0
+    //private var promedio : Float = 0.0F
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,6 +34,12 @@ class ResenaFragment : Fragment() {
 
         binding = FragmentResenaBinding.bind(view)
 
+        var puntuacionActual: Int = 0
+        var puntuacionLibro: Int = 0
+        var cantidadDePuntuaciones: Int = 0
+        var puntuacionPromedio: Float = 0.0F
+
+
         val args: ResenaFragmentArgs by navArgs()
         val libroDetalle = args.libroDetalle
 
@@ -36,11 +48,53 @@ class ResenaFragment : Fragment() {
         val user = FirebaseAuth.getInstance().currentUser
         user?.let {
             val nombreUsuario = user.email
+            val uidUsuario = user.uid
             binding.userTextView.text = nombreUsuario.toString()
-        }
 
-        binding.enviarButton.setOnClickListener {
+            binding.resenaRatingBar.setOnRatingBarChangeListener { ratingBar, puntuacion, b ->
+                puntuacionActual = puntuacion.toInt()
+                puntuacionLibro = puntuacionActual + libroDetalle.puntuacion
+                cantidadDePuntuaciones = libroDetalle.cantidadDePuntuaciones + 1
+                puntuacionPromedio = (puntuacionLibro / cantidadDePuntuaciones).toFloat()
+            }
 
+            binding.enviarButton.setOnClickListener {
+
+                val comentario = binding.comentarioEditText.text.toString()
+                actualizarPuntuacionLibroFirebase(
+                    uidUsuario,
+                    libroDetalle.id.toString(),
+                    puntuacionLibro,
+                    cantidadDePuntuaciones,
+                    puntuacionPromedio,
+                    comentario
+                )
+                /*actualizarPuntuacionUsuarioFirebase(
+                     uidUsuario,
+                     libroDetalle.id.toString(),
+                     puntuacionActual
+                 )*/
+                Toast.makeText(context, "Reseña guardada", Toast.LENGTH_SHORT).show()
+            }
         }
+    }
+
+    private fun actualizarPuntuacionLibroFirebase(
+        uidUsuario: String,
+        idLibro: String,
+        puntuacionLibro: Int,
+        cantidadDePuntuaciones: Int,
+        puntuacionPromedio: Float,
+        comentario: String
+    ) {
+        val database = FirebaseDatabase.getInstance()
+        val myUsuarioRef = database.getReference("libros")
+        val childUpdates = HashMap<String, Any>()
+        childUpdates["puntuacion"] = puntuacionLibro
+        childUpdates["cantidadDePuntuaciones"] = cantidadDePuntuaciones
+        childUpdates["promedio"] = puntuacionPromedio
+        childUpdates["comentario"] = comentario
+        idLibro.let { myUsuarioRef.child(it).updateChildren(childUpdates) }
+        Toast.makeText(context, "Puntuación almacenada", Toast.LENGTH_SHORT).show()
     }
 }
