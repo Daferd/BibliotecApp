b62cede329380dfc93d1d570556d8b735d6360ec
Dario Arevalo <daferd19@gmail.com>
2020-12-15 16:59:33 -0500

    Implementacion vista de administrador con Navigation Component

--- app/src/main/java/com/daferarevalo/bibliotecapp/ui/administrador/AdministradorBottomActivity.kt
+++ app/src/main/java/com/daferarevalo/bibliotecapp/ui/administrador/AdministradorBottomActivity.kt
@@ -2,16 +2,18 @@ package com.daferarevalo.bibliotecapp.ui.administrador
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.daferarevalo.bibliotecapp.R
 import com.daferarevalo.bibliotecapp.databinding.ActivityAdministradorBottomBinding
-import com.daferarevalo.bibliotecapp.ui.administrador.reservas.ReservasFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class AdministradorBottomActivity : AppCompatActivity() {
 
-    private val reservasFragment = ReservasFragment()
-    private val prestamosFragment = PrestamosFragment()
+    //private val reservasFragment = ReservasFragment()
+    //private val prestamosFragment = PrestamosFragment()
 
 
     private lateinit var binding: ActivityAdministradorBottomBinding
@@ -21,24 +23,30 @@ class AdministradorBottomActivity : AppCompatActivity() {
         binding = ActivityAdministradorBottomBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-        replaceFregment(reservasFragment)
 
-        binding.bottomNavigation.setOnNavigationItemSelectedListener {
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        val navController: NavController = Navigation.findNavController(this, R.id.nav_host_frag)
+
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
+        //replaceFregment(reservasFragment)
+
+        /*binding.bottomNavigation.setOnNavigationItemSelectedListener {
             when (it.itemId) {
                 R.id.reservasFragment -> replaceFregment(reservasFragment)
                 R.id.prestamosFragment -> replaceFregment(prestamosFragment)
             }
             true
-        }
+        }*/
 
 
     }
 
-    private fun replaceFregment(fragment: Fragment) {
+    /*private fun replaceFregment(fragment: Fragment) {
         if (fragment != null) {
             val transition = supportFragmentManager.beginTransaction()
             transition.replace(R.id.frame_container, fragment)
             transition.commit()
         }
-    }
+    }*/
 }
